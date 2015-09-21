247b8f48c0c33efacb5649c2c9fe9d4472939b33
noboru-i <ishikura.noboru@gmail.com>
2015-09-21 22:55:29 +0900

    using babel in test

--- test/webhook/index.js
+++ test/webhook/index.js
@@ -1,15 +1,22 @@
 var assert = require("assert");
 var rewire = require('rewire');
 
-var module = rewire('../../actions/webhook/index.js');
+var subject = rewire('../../actions/webhook/index.js');
 
-module.__set__('BACKLOG_URL', 'http://foo.backlog.jp/');
+subject.__set__('BACKLOG_URL', 'http://foo.backlog.jp/');
 
 describe('#create_link()', function() {
-  var create_link = module.__get__('create_link');
-  it('should return issue link when comment element not exists.', function () {
+  var create_link = subject.__get__('create_link');
+
+  it('should return issue link when comment element not exists.', () => {
     var event_obj = {"project": {"projectKey": "KEY"}};
-    var content = {"key_id": "1234"};
+    var content = {"key_id": 1234};
     assert.equal(create_link(event_obj, content), "http://foo.backlog.jp/view/KEY-1234");
+  });
+
+  it('should return comment link when comment element exists.', () => {
+    var event_obj = {"project": {"projectKey": "KEY"}};
+    var content = {"key_id": 1234, "comment": {"id": 999}};
+    assert.equal(create_link(event_obj, content), "http://foo.backlog.jp/view/KEY-1234#comment-999");
   });
 });
